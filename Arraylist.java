7a873bf3210dccf587ca2c55a88ed592453a16cf
kanisudha <115447951+kanisudha@users.noreply.github.com>
2022-10-25 23:50:02 +0530

    Add files via upload
    new array

---
+++ Arraylist.java
@@ -0,0 +1,28 @@
+package arraytest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List l=new ArrayList();
+		
+		l.add(10);
+		l.add(30);
+		l.add(40);
+		l.add(8);
+		l.add(98);
+		int length=l.size();
+		System.out.println(l);
+
+		
+		
+		
+		
+		
+	}
+
+}
